7c64db2ba77f3413265790bcd9216e9d88b94dce
Andrew <andrew@andrewcbaker.net>
2020-08-23 13:18:27 -0700

    Prevent Towny from despawning Appa
    Took 22 minutes

--- src/main/java/me/relavis/avatarcreatures/AvatarCreatures.java
+++ src/main/java/me/relavis/avatarcreatures/AvatarCreatures.java
@@ -37,16 +37,16 @@ public final class AvatarCreatures extends JavaPlugin implements Listener {
         DataHandler data = new DataHandler();
         data.dataSetup();
 
-        Bukkit.getPluginManager().registerEvents(new SpawnItemClickEvent(), this);
-        Bukkit.getPluginManager().registerEvents(new PlayerDamagedEvent(), this);
         Bukkit.getPluginManager().registerEvents(new MountEntityDamagedEvent(), this);
+        Bukkit.getPluginManager().registerEvents(new MountEntityDeathEvent(), this);
         Bukkit.getPluginManager().registerEvents(new MountEntityHostileTargetEvent(), this);
         Bukkit.getPluginManager().registerEvents(new MountEntityMountEvent(), this);
         Bukkit.getPluginManager().registerEvents(new MountEntityUnmountEvent(), this);
-        Bukkit.getPluginManager().registerEvents(new MountEntityDeathEvent(), this);
-        Bukkit.getPluginManager().registerEvents(new PlayerQuitEvent(), this);
+        Bukkit.getPluginManager().registerEvents(new PlayerDamagedEvent(), this);
         Bukkit.getPluginManager().registerEvents(new PlayerJoinEvent(), this);
-
+        Bukkit.getPluginManager().registerEvents(new PlayerQuitEvent(), this);
+        Bukkit.getPluginManager().registerEvents(new SpawnItemClickEvent(), this);
+        Bukkit.getPluginManager().registerEvents(new TownyMobRemovalEvent(), this);
 
         Objects.requireNonNull(this.getCommand("appa")).setExecutor(new AppaCommand());
 
---
+++ src/main/java/me/relavis/avatarcreatures/events/TownyMobRemovalEvent.java
@@ -0,0 +1,21 @@
+package me.relavis.avatarcreatures.events;
+
+import com.palmergames.bukkit.towny.event.MobRemovalEvent;
+import me.relavis.avatarcreatures.util.DataHandler;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+import java.util.UUID;
+
+public class TownyMobRemovalEvent implements Listener {
+
+    DataHandler data = DataHandler.getInstance();
+
+    @EventHandler
+    public void onTownyMobRemove(MobRemovalEvent e) {
+        UUID entityUUID = e.getEntity().getUniqueId();
+        if (data.entityHasData(entityUUID)) {
+            e.setCancelled(true);
+        }
+    }
+}
